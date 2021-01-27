796b689f67fad5d977320eb29b3a2b07fcae9932
quoctrung163 <miyucoder@gmail.com>
2021-01-27 08:57:11 +0700

    change name model in lecture controller

--- src/server/controllers/lecture.ts
+++ src/server/controllers/lecture.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 
-import Course from '../models/Course';
-import Lecture from '../models/Lecture';
+import courseModel from '../models/Course';
+import lectureModel from '../models/Lecture';
 
 import { findCourseByNameService } from '../services/enrollment';
 import { getLecturesService, saveLectureService } from '../services/lecture';
 
 export const getLecturesCtrl = (req: Request, res: Response) => {
-    getLecturesService(Lecture, req.query.id)
+    getLecturesService(lectureModel, req.query.id)
         .then((doc) => {
             res.json(doc);
         })
@@ -17,7 +17,7 @@ export const getLecturesCtrl = (req: Request, res: Response) => {
 };
 
 export const postVideosCtrl = (req: Request, res: Response) => {
-    findCourseByNameService(Course, req.body.course, (error: any, cat: any) => {
+    findCourseByNameService(courseModel, req.body.course, (error: any, cat: any) => {
         if (!error && cat) {
             req.body.course = cat[0]._id;
         }
@@ -35,7 +35,7 @@ export const postVideosCtrl = (req: Request, res: Response) => {
             console.log(req.body.videoLink);
         }
 
-        const upload = new Lecture(req.body);
+        const upload = new lectureModel(req.body);
         saveLectureService(upload);
         res.send('This is Post Route Upload');
     });
@@ -46,7 +46,7 @@ export const postVideosYoutubeCtrl = (req: Request, res: Response) => {
         return res.status(400).send('Request body is missing');
     }
 
-    const model = new Lecture(req.body);
+    const model = new lectureModel(req.body);
     saveLectureService(model)
         .then((doc) => {
             if (!doc || doc.length === 0) {
