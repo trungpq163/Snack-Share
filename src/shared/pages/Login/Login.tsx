4c6add2516722795b00e4b07d5dd844c033531c4
quoctrung163 <miyucoder@gmail.com>
2021-02-02 11:21:43 +0700

    refactor Login ->>>>>> CLEAN

--- src/shared/pages/Login/Login.tsx
+++ src/shared/pages/Login/Login.tsx
@@ -1,14 +1,11 @@
 import React, { FormEvent, useState } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import { loginUser } from '../../store/auth/effects';
 
-import { LinkCustom, LinkCustomActive } from '../../styles/LinkCustom.Styles';
-
-import 'react-toastify/dist/ReactToastify.css';
-import '../../styles/Form.Styles.css';
+import LoginDumb from '../../components/Login/Login';
 
 interface User {
     email: string;
@@ -57,71 +54,12 @@ const Login = ({ match }: any) => {
     };
 
     return (
-        <div className="signup-signin">
-            <img src="https://imgur.com/aILP3CD.png" alt="login" className="signup-signin-image" />
-            <div className="signup-signin-container">
-                <div className="tab">
-                    <div className="tab-item">
-                        <LinkCustom to={`/register/${roleParams}`}>Sign up</LinkCustom>
-                    </div>
-                    <div className="tab-item is-active">
-                        <LinkCustomActive to={`/login/${roleParams}`}>Sign in</LinkCustomActive>
-                    </div>
-                </div>
-                <h1 className="signup-signin-heading">Sign in</h1>
-                <form
-                    action="post"
-                    className="signup-signin-form"
-                    autoComplete="off"
-                    onSubmit={clickSubmit}
-                >
-                    <div className="form-group">
-                        <label htmlFor="email" className="form-label">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            className="form-input"
-                            placeholder="Ex: johndoe@email.com"
-                            value={values.email}
-                            onChange={handleChange('email')}
-                            required
-                            name="email"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password" className="form-label">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            className="form-input"
-                            placeholder="************"
-                            value={values.password}
-                            onChange={handleChange('password')}
-                            required
-                            name="password"
-                        />
-                    </div>
-                    <div className="form-group signup-signin-term">
-                        Don’t have an account?{' '}
-                        <Link className="signup-signin-term-link" to="/register/student">
-                            Sign up (student)
-                        </Link>
-                        {' / '}
-                        <Link className="signup-signin-term-link" to="/register/instructor">
-                            Sign up (instructor)
-                        </Link>
-                    </div>
-                    <button type="submit" className="btn btn--gradient">
-                        Sign in
-                    </button>
-                </form>
-                <ToastContainer />
-            </div>
-        </div>
+        <LoginDumb
+            roleParams={roleParams}
+            clickSubmit={clickSubmit}
+            handleChange={handleChange}
+            values={values}
+        />
     );
 };
 
