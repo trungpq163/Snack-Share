9c83506c4136e3a34ded3e2e57301b86ec5d3f99
quoctrung163 <miyucoder@gmail.com>
2021-02-02 11:41:33 +0700

    refactor Register ->>>>>> CLEAN

---
+++ src/shared/types/index.ts
@@ -0,0 +1,10 @@
+export interface User {
+    // eslint-disable-next-line camelcase
+    first_name: string;
+    // eslint-disable-next-line camelcase
+    last_name: string;
+    email: string;
+    password: string;
+    password2: string;
+    role?: string;
+}
