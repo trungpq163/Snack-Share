c47ee829b785bdc761cb33ec23a250bd720a6735
quoctrung163 <miyucoder@gmail.com>
2021-02-03 10:15:56 +0700

    edit .gitignore to upload build to git

--- src/shared/components/Partners/Partners.Styles.css.d.ts
+++ src/shared/components/Partners/Partners.Styles.css.d.ts
@@ -1,8 +1,9 @@
 declare const styles: {
-    readonly 'block-title__title': string;
-    readonly 'brand-one': string;
-    readonly 'brand-one__carousel': string;
-    readonly 'brand-one__home-three': string;
-    readonly 'brand-two': string;
+  readonly "block-title__title": string;
+  readonly "brand-one": string;
+  readonly "brand-one__carousel": string;
+  readonly "brand-one__home-three": string;
+  readonly "brand-two": string;
 };
 export = styles;
+
