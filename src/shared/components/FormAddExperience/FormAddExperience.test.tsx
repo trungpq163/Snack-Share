35246badc1f2e7ef10f101ae2d9d10c6e3032cb2
quoctrung163 <miyucoder@gmail.com>
2021-02-06 20:24:26 +0700

    basic show category with :heart:

--- src/shared/components/FormAddExperience/FormAddExperience.test.tsx
+++ src/shared/components/FormAddExperience/FormAddExperience.test.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 
 import FormAddExperience from './FormAddExperience';
+import any from '*.css';
 
 describe('FormAddExperience', () => {
-    const defaultProps = {};
+    const defaultProps = any;
 
     it('tests something', () => {
         shallow(<FormAddExperience {...defaultProps} />);
--- src/shared/components/ShowCategory/ShowCategory.tsx
+++ src/shared/components/ShowCategory/ShowCategory.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './ShowCategory.Styles.css';
 
 const ShowCategory = ({ category }: any) => {
     console.log('category', category);
@@ -14,8 +15,35 @@ const ShowCategory = ({ category }: any) => {
             }}
         >
             <div className="block-title" style={{ textAlign: 'center' }}>
-                <h3>{'Category List :<'}</h3>
+                <a
+                    className="link-action__category"
+                    style={{ fontSize: '1.66rem', cursor: 'pointer' }}
+                >
+                    Add Category
+                </a>
             </div>
+            <table className="table" style={{ width: '70%' }}>
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {category?.map((child: any, index: any) => (
+                        <tr key={index}>
+                            <th scope="row">{index}</th>
+                            <td>{child?.categoryName}</td>
+                            <td>
+                                <a className="link-action__category" href="#">
+                                    Edit
+                                </a>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
