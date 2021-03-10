79a24185ae7ea5cfc65742f9dae7714424545220
quoctrung163 <miyucoder@gmail.com>
2021-03-10 12:18:51 +0700

    remove comment with :heart:

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,7 +8,6 @@ import manifestHelpers from 'express-manifest-helpers';
 import mongoose from 'mongoose';
 import fileUpload from 'express-fileupload';
 import passport from 'passport';
-// import stripe from 'config/stripe';
 import paths from '../../config/paths';
 // import { configureStore } from '../shared/store';
 import errorHandler from './middleware/errorHandler';
@@ -62,35 +61,6 @@ app.get('/locales/refresh', webhookVerification, refreshTranslations);
 app.get('/locales/:locale/:ns.json', i18nextXhr);
 
 app.get('/helloworld', (_req, res) => res.send('Hello World'));
-
-// Hook Point is https://snack-dev.herokuapp.com/webhook OK
-// app.post('/webhook', express.raw({ type: 'application/json' }), (req: Request, res: Response) => {
-//     const event = req.body;
-//     // Handle the event
-//     switch (event.type) {
-//         case 'payment_intent.succeeded': {
-//             const paymentIntent = event.data.object;
-//             console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
-//             // Then define and call a method to handle the successful payment intent.
-//             // handlePaymentIntentSucceeded(paymentIntent);
-//             console.log(`Req body ${paymentIntent.id}`);
-//             break;
-//         }
-//         case 'payment_method.attached': {
-//             const paymentMethod = event.data.object;
-//             // Then define and call a method to handle the successful attachment of a PaymentMethod.
-//             // handlePaymentMethodAttached(paymentMethod);
-//             console.log(paymentMethod);
-//             break;
-//         }
-//         default: {
-//             // Unexpected event type
-//             console.log(`Unhandled event type ${event.type}.`);
-//         }
-//     }
-//     // Return a 200 response to acknowledge receipt of the event
-//     res.sendStatus(200);
-// });
 
 // Mount routes
 app.use('/api/', categoryRoute);
