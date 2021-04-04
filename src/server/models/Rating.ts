1afa59e8bb32b221a23ee9a709a83f418be52176
quoctrung163 <miyucoder@gmail.com>
2021-04-04 08:39:14 +0700

    add Rating model with :heart:

---
+++ src/server/models/Rating.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { UserInterface } from './User';
+import { CourseInterface } from './Course';
+
+export interface RatingInterface extends Document {
+    user: UserInterface['_id'];
+    course: CourseInterface['_id'];
+    content: string;
+    star: number;
+}
+
+const RatingSchema: Schema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+    },
+    course: {
+        type: Schema.Types.ObjectId,
+        ref: 'course',
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    star: {
+        type: Number,
+        required: true,
+    },
+});
+
+export default mongoose.model<RatingInterface>('ratings', RatingSchema);
