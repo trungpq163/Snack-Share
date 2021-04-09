0e69e9b255926448c0e7fb9e7e8fa75dd076489a
quoctrung163 <miyucoder@gmail.com>
2021-04-09 16:36:24 +0700

    fix selectors.test.ts

--- src/shared/store/app/selectors.test.ts
+++ src/shared/store/app/selectors.test.ts
@@ -7,6 +7,6 @@ const state = {
 
 describe('App Selectors', () => {
     it('gets the locale', () => {
-        expect(getLocale(state)).toMatch('en_US');
+        expect(getLocale(state)).toMatch('vi_VN');
     });
 });
