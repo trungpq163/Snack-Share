37f4e94887a632d9319dfbe67783595e482daf70
quoctrung163 <miyucoder@gmail.com>
2021-04-09 16:19:49 +0700

    fix test language

--- src/shared/store/app/reducer.test.ts
+++ src/shared/store/app/reducer.test.ts
@@ -2,7 +2,15 @@ import reducer, { initialState } from './reducer';
 import { ActionTypes } from './actions';
 
 describe('App Reducer', () => {
-    it('sets the locale', () => {
+    it('check english', () => {
+        expect(reducer(initialState, { type: ActionTypes.SETLOCALE, payload: 'en_US' })).toEqual({
+            locale: 'en_US',
+        });
+    });
+});
+
+describe('App Reducer', () => {
+    it('check vietnam', () => {
         expect(reducer(initialState, { type: ActionTypes.SETLOCALE, payload: 'vi_VN' })).toEqual({
             locale: 'vi_VN',
         });
